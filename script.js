8ed674ca4cb061dab3d60ff147af1560b8c81721
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2024-09-03 00:33:24 -0400

    Atualizações site exercicio

--- script.js
+++ script.js
@@ -18,5 +18,67 @@ const initTypingAnimation = () => {
     typingAnimation(span, 'Prf: Enilda Aparecida Mendes da Rosa Cáceres', 1600)
     typingAnimation(paragraph, 'Guilherme Machado e Juliana Barbosa', 4200)
 }
-
 initTypingAnimation()
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const sr = ScrollReveal({ reset: true }, {mobile: true,});
+
+    sr.reveal('.containerMainIntroducao', {
+        rotate: { x: 0, y: 80, z: 0 },
+        duration: 3000,
+        reset: false,
+        mobile: true,
+    });
+
+    sr.reveal('.containerTitleSemantica', {
+        distance: '3000px',
+        origin: 'top',
+        duration: 1000,
+        delay: 100,
+        reset: false,
+        mobile: true,
+    });
+    sr.reveal('.mainSemantica', {
+        distance: '3000px',
+        origin: 'left',
+        duration: 1000,
+        delay: 100,
+        reset: false,
+        mobile: true,
+    });
+    sr.reveal('#titleTag', {
+        distance: '3000px',
+        origin: 'top',
+        duration: 1000,
+        delay: 100,
+        reset: false,
+        mobile: true,
+    });
+    sr.reveal('.containerConteudoTags', {
+        distance: '3000px',
+        origin: 'left',
+        duration: 1000,
+        delay: 100,
+        reset: false,
+        mobile: true,
+    });
+
+    sr.reveal('.containerDescTag h1, .containerDescTag h2', {
+        distance: '3000px',
+        origin: 'left',
+        duration: 1000,
+        delay: 100,
+        reset: false,
+        mobile: true,
+    });
+
+    sr.reveal('.card', {
+        distance: '3000px',
+        origin: 'top',
+        duration: 1000,
+        delay: 100,
+        reset: false,
+        mobile: true,
+    });
+});
